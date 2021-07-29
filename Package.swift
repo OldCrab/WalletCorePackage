db134e893e5cf8c91c71133a8c3e95b5c92f4273
Vasiliev Andrey <scrapland0@gmail.com>
2021-07-29 12:47:44 +0300

    new changes

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/OldCrab/WalletCorePackage/releases/download/0.1/WalletCore.xcframework.zip",
-            checksum: "457027b1d383d8bfa37f9e8039106fde166d731752e2ee426f69f7608d49f6b0"
+            checksum: "fc9752de863dcb10ae3c3c21028848bd1740e3451cb21cdf311fb0962ea44e02"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
             url: "https://github.com/OldCrab/WalletCorePackage/releases/download/0.1/SwiftProtobuf.xcframework.zip",
-            checksum: "9f023fe370d5d6d3bc0df49c200c77b9ddbd165f76f2dbfbdba5393a43df327d"
+            checksum: "66eb6206f0cac5e78a6b2db2a35ed7f7e53649bacb0d830da2778a241d87a74b"
         )
     ]
 )
